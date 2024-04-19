51e7c5cf6fecb56844a848958a554796287dd0e3
AdrienneP-hoenix86 <adriennepallett@icloud.com>
2024-04-19 08:58:03 -0400

    My First Java Program
    This was my first program I wrote in Java. It's purpose was to demonstrate the ability to print lines in the console reflecting personal details.

---
+++ pDataAdrienne.java
@@ -0,0 +1,49 @@
+
+public class pDataAdrienne {
+
+	public pDataAdrienne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name: Adrienne Pallett");
+		
+		//  2) Email address 
+		System.out.println("Email: ag3358@mynsu.nova.edu");
+		
+		//  3) Major
+		System.out.println("Major: Information Technology");
+		
+		// 4) Minor (if any. If none then put N/A)
+		System.out.println("Minor: Graphic Design / Cybersecurity");
+		
+		// 5) Status at Nova (full-time, part-time, non-degree seeking) 
+		System.out.println("Status at Nova: Full-time");
+		
+		//  6) Employment status (full-time, part-time, intern, student) 
+		System.out.println("Employment Status: Student");
+		
+		//  7) Campus dorm or town where you now live
+		System.out.println("Where do you live: Plantation");
+		
+		//  8) Hometown where you grew up 
+		System.out.println("Hometown: Plantation FL");
+		
+		//  9) Previous programming course(s) taken, where, and instructor’s name 
+		System.out.println("Previous Programming Courses:");
+		System.out.println("-- Art Institute (Dallas) - HTML, CSS, JavaScript - Mullins / Prince");
+		System.out.println("-- Nova Southeastern - Python, SQL - Harward / Kumar");
+		
+		//  10) Programming languages with which you have even a little experience
+		System.out.println("Programming Languages: HTML, CSS, JS, SQL, Python");
+		
+		// 11) Favorite Movie or Favorite TV Show you like to watch
+		System.out.println("Favorite Movie / TV Show: Hobbit/Lord of the Rings Saga");
+		
+		// 12) Favorite Hobby
+		System.out.println("Favorite Hobby: Dancing, Hockey");
+	}
+
+}
